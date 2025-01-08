d916175f16487eb743620d6eb09863b8bf19bf7c
Yasmeen Allahaleh <yasallahaleh@gmail.com>
2025-01-08 09:40:58 -0800

    Add files via upload

---
+++ Q3_AITestOutputFormat/main.cpp
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: YasmeenAllahaleh
+ *
+ * Created on February 22, 2024, 6:29 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    //Declared Variables
+    int num;
+    float num1;
+    
+    //Input for each variable
+    cin >> num;
+    cin >> num1;
+   
+    //Output for Display
+    cout << num << endl << num1 << endl << "Hello World     " << endl << "\tTab it!" << endl
+    << "Compare . . . to space   ";
+    
+    return 0;
+}
+
