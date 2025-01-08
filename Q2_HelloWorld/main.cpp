dceecb0380156cca9ca5c4fad2e8936fa2f256bd
Yasmeen Allahaleh <yasallahaleh@gmail.com>
2025-01-08 09:40:40 -0800

    Add files via upload

---
+++ Q2_HelloWorld/main.cpp
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: YasmeenAllahaleh
+ *
+ * Created on February 22, 2024, 6:23 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+//Set Random Number Seed Here
+    
+    //Declare all Variables Here
+    
+    //Input or initialize values Here
+    
+    //Process/Calculations Here
+    
+    //Output Located Here
+    cout << endl << "   Hello World!  " << endl << endl;
+    //Exit
+    
+    return 0;
+}
+
---
+++ Q2_HelloWorld/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=q2_helloworld
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/q2_helloworld
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=q2helloworld.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/q2helloworld.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=q2_helloworld
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/q2_helloworld
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=q2helloworld.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/q2helloworld.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
