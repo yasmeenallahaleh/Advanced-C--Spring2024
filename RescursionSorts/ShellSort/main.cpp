b340da42bebddbcf1a5960cd5b2a967a8985515e
Yasmeen Allahaleh <yasallahaleh@gmail.com>
2025-01-08 09:49:21 -0800

    Add files via upload

---
+++ RescursionSorts/ShellSort/main.cpp
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Yasmeen Allahaleh
+ *
+ * Created on May 3, 2024, 3:38 PM
+ *///Website: https://www.programiz.com/dsa/shell-sort 
+
+// Shell Sort in C++ programming
+
+#include <iostream>
+using namespace std;
+
+// Shell sort
+void shellSort(int array[], int n) {
+  // Rearrange elements at each n/2, n/4, n/8, ... intervals
+  for (int interval = n / 2; interval > 0; interval /= 2) {
+    for (int i = interval; i < n; i += 1) {
+      int temp = array[i];
+      int j;
+      for (j = i; j >= interval && array[j - interval] > temp; j -= interval) {
+        array[j] = array[j - interval];
+      }
+      array[j] = temp;
+    }
+  }
+}
+
+// Print an array
+void printArray(int array[], int size) {
+  int i;
+  for (i = 0; i < size; i++)
+    cout << array[i] << " ";
+  cout << endl;
+}
+
+// Driver code
+int main() {
+  int data[] = {9, 8, 3, 7, 5, 6, 4, 1};
+  int size = sizeof(data) / sizeof(data[0]);
+  shellSort(data, size);
+  cout << "Sorted array: \n";
+  printArray(data, size);
+}
+
