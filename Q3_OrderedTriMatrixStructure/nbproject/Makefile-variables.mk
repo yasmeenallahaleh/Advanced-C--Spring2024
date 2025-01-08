ba8e1417ea8459ec17817156c05d982eccb8bb8f
Yasmeen Allahaleh <yasallahaleh@gmail.com>
2025-01-08 09:43:30 -0800

    Add files via upload

---
+++ Q3_OrderedTriMatrixStructure/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=q3_orderedtrimatrixstructure
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/q3_orderedtrimatrixstructure
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=q3orderedtrimatrixstructure.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/q3orderedtrimatrixstructure.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=q3_orderedtrimatrixstructure
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/q3_orderedtrimatrixstructure
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=q3orderedtrimatrixstructure.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/q3orderedtrimatrixstructure.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
